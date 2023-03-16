794a03f9ddb911e9452d1817b855b59acbc97ef1
Ka Long Ngai <kls8162@gmail.com>
2023-03-16 17:06:16 -0400

    Project_2

---
+++ Project2/script.js
@@ -0,0 +1,53 @@
+function MrO() {
+    var x = document.getElementById("MrO");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function TS() {
+    var x = document.getElementById("TS");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function OS() {
+    var x = document.getElementById("OS");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function WM() {
+    var x = document.getElementById("WM");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function EP() {
+    var x = document.getElementById("EP");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function GG() {
+    var x = document.getElementById("GG");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
